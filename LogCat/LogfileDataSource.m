79337cc4e3af9e275145b82e60fa3e1a5ef7f959
Jan Rose <janrose.1986@googlemail.com>
2014-04-25 23:03:36 +0200

    added parsing of lines with all available patterns

--- LogCat/LogfileDataSource.m
+++ LogCat/LogfileDataSource.m
@@ -101,45 +101,35 @@
     [fileReader enumerateLinesUsingBlock:^(NSString* line, BOOL* stop) {
         NSLog(@"line: %@", line);
         //parse line
-        NSUInteger numberOfMatches = [regExpTime numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
-        NSLog(@"number of matches: %lu", (unsigned long)numberOfMatches);
+        BOOL parseSuccess = [self tryReadWithTimePatternForLine:line];
         
-        if(numberOfMatches == 1)
-        {
-            NSArray *matches = [regExpTime matchesInString:line
-                                                   options:0
-                                                     range:NSMakeRange(0, [line length])];
-            
-            NSTextCheckingResult *match = [matches objectAtIndex:0];
-            
-            NSRange timestampRange = [match rangeAtIndex:1];
-            NSString *timestamp = [line substringWithRange:timestampRange];
-            
-            NSRange levelRange = [match rangeAtIndex:2];
-            NSString* level = [line substringWithRange:levelRange];
-            
-            NSRange tagRange = [match rangeAtIndex:3];
-            NSString* tag = [line substringWithRange:tagRange];
-            
-            NSRange pidRange = [match rangeAtIndex:4];
-            NSString* pid = [line substringWithRange:pidRange];
-            
-            NSRange msgRange = [match rangeAtIndex:5];
-            NSString* msg = [line substringWithRange:msgRange];
-            
-            //time, app, pid, tid, type, name, text,
-            NSArray* values = @[[self getIndex], timestamp, @"", pid, @"", level, tag, msg];
-            NSLog(@"entry: %@", values);
-            NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
-            [self appendRow:row];
+        if (!parseSuccess) {
+            parseSuccess = [self tryReadWithBriefPatternForLine:line];
         }
         
-        //TODO:
+        if (!parseSuccess) {
+            parseSuccess = [self tryReadWithThreadtimePatternForLine:line];
+        }
         
+        if (!parseSuccess) {
+            parseSuccess = [self tryReadWithThreadPatternForLine:line];
+        }
         
+        if (!parseSuccess) {
+            parseSuccess = [self tryReadWithTagPatternForLine:line];
+        }
         
+        if (!parseSuccess) {
+            parseSuccess = [self tryReadWithProcessPatternForLine:line];
+        }
         
+        if (!parseSuccess) {
+            parseSuccess = [self tryReadWithDdmsSavePatternForLine:line];
+        }
         
+        if (!parseSuccess) {
+            NSLog(@"could not read line: %@", line);
+        }
         
     }];
     
@@ -157,4 +147,260 @@
 //    self.skipPidLookup = NO;
 }
 
+- (BOOL)tryReadWithTimePatternForLine:(NSString*)line
+{
+    NSUInteger numberOfMatches = [regExpTime numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
+    
+    if(numberOfMatches == 1)
+    {
+        NSArray *matches = [regExpTime matchesInString:line
+                                               options:0
+                                                 range:NSMakeRange(0, [line length])];
+        
+        NSTextCheckingResult *match = [matches objectAtIndex:0];
+        
+        NSRange timestampRange = [match rangeAtIndex:1];
+        NSString *timestamp = [line substringWithRange:timestampRange];
+        
+        NSRange levelRange = [match rangeAtIndex:2];
+        NSString* level = [line substringWithRange:levelRange];
+        
+        NSRange tagRange = [match rangeAtIndex:3];
+        NSString* tag = [line substringWithRange:tagRange];
+        
+        NSRange pidRange = [match rangeAtIndex:4];
+        NSString* pid = [line substringWithRange:pidRange];
+        
+        NSRange msgRange = [match rangeAtIndex:5];
+        NSString* msg = [line substringWithRange:msgRange];
+        
+        //time, app, pid, tid, type, name, text,
+        NSArray* values = @[[self getIndex], timestamp, @"", pid, @"", level, tag, msg];
+        NSLog(@"entry: %@", values);
+        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
+        [self appendRow:row];
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (BOOL)tryReadWithBriefPatternForLine:(NSString*)line
+{
+    NSUInteger numberOfMatches = [regExpBrief numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
+    NSLog(@"number of matches: %lu", (unsigned long)numberOfMatches);
+    
+    if(numberOfMatches == 1)
+    {
+        NSArray *matches = [regExpBrief matchesInString:line
+                                               options:0
+                                                 range:NSMakeRange(0, [line length])];
+        
+        NSTextCheckingResult *match = [matches objectAtIndex:0];
+        
+        NSRange levelRange = [match rangeAtIndex:1];
+        NSString* level = [line substringWithRange:levelRange];
+        
+        NSRange tagRange = [match rangeAtIndex:2];
+        NSString* tag = [line substringWithRange:tagRange];
+        
+        NSRange pidRange = [match rangeAtIndex:3];
+        NSString* pid = [line substringWithRange:pidRange];
+        
+        NSRange msgRange = [match rangeAtIndex:4];
+        NSString* msg = [line substringWithRange:msgRange];
+        
+        //time, app, pid, tid, type, name, text,
+        NSArray* values = @[[self getIndex], @"", @"", pid, @"", level, tag, msg];
+        NSLog(@"entry: %@", values);
+        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
+        [self appendRow:row];
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (BOOL)tryReadWithThreadtimePatternForLine:(NSString*)line
+{
+    NSUInteger numberOfMatches = [regExpThreadtime numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
+    NSLog(@"number of matches: %lu", (unsigned long)numberOfMatches);
+    
+    if(numberOfMatches == 1)
+    {
+        NSArray *matches = [regExpThreadtime matchesInString:line
+                                                options:0
+                                                  range:NSMakeRange(0, [line length])];
+        
+        NSTextCheckingResult *match = [matches objectAtIndex:0];
+        
+        NSRange timestampRange = [match rangeAtIndex:1];
+        NSString *timestamp = [line substringWithRange:timestampRange];
+        
+        NSRange pidRange = [match rangeAtIndex:2];
+        NSString* pid = [line substringWithRange:pidRange];
+        
+        NSRange tidRange = [match rangeAtIndex:3];
+        NSString* tid = [line substringWithRange:tidRange];
+        
+        NSRange levelRange = [match rangeAtIndex:4];
+        NSString* level = [line substringWithRange:levelRange];
+        
+        NSRange tagRange = [match rangeAtIndex:5];
+        NSString* tag = [line substringWithRange:tagRange];
+        
+        NSRange msgRange = [match rangeAtIndex:6];
+        NSString* msg = [line substringWithRange:msgRange];
+        
+        //time, app, pid, tid, type, name, text,
+        NSArray* values = @[[self getIndex], timestamp, @"", pid, tid, level, tag, msg];
+        NSLog(@"entry: %@", values);
+        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
+        [self appendRow:row];
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (BOOL)tryReadWithProcessPatternForLine:(NSString*)line
+{
+    NSUInteger numberOfMatches = [regExpProcess numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
+    NSLog(@"number of matches: %lu", (unsigned long)numberOfMatches);
+    
+    if(numberOfMatches == 1)
+    {
+        NSArray *matches = [regExpProcess matchesInString:line
+                                                     options:0
+                                                       range:NSMakeRange(0, [line length])];
+        
+        NSTextCheckingResult *match = [matches objectAtIndex:0];
+        
+        NSRange levelRange = [match rangeAtIndex:1];
+        NSString* level = [line substringWithRange:levelRange];
+        
+        NSRange pidRange = [match rangeAtIndex:2];
+        NSString* pid = [line substringWithRange:pidRange];
+        
+        NSRange msgRange = [match rangeAtIndex:3];
+        NSString* msg = [line substringWithRange:msgRange];
+        
+        //time, app, pid, tid, type, name, text,
+        NSArray* values = @[[self getIndex], @"", @"", pid, @"", level, @"", msg];
+        NSLog(@"entry: %@", values);
+        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
+        [self appendRow:row];
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (BOOL)tryReadWithTagPatternForLine:(NSString*)line
+{
+    NSUInteger numberOfMatches = [regExpTag numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
+    NSLog(@"number of matches: %lu", (unsigned long)numberOfMatches);
+    
+    if(numberOfMatches == 1)
+    {
+        NSArray *matches = [regExpTag matchesInString:line
+                                                  options:0
+                                                    range:NSMakeRange(0, [line length])];
+        
+        NSTextCheckingResult *match = [matches objectAtIndex:0];
+        
+        NSRange levelRange = [match rangeAtIndex:1];
+        NSString* level = [line substringWithRange:levelRange];
+        
+        NSRange tagRange = [match rangeAtIndex:2];
+        NSString* tag = [line substringWithRange:tagRange];
+        
+        NSRange msgRange = [match rangeAtIndex:3];
+        NSString* msg = [line substringWithRange:msgRange];
+        
+        //time, app, pid, tid, type, name, text,
+        NSArray* values = @[[self getIndex], @"", @"", @"", @"", level, tag, msg];
+        NSLog(@"entry: %@", values);
+        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
+        [self appendRow:row];
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (BOOL)tryReadWithThreadPatternForLine:(NSString*)line
+{
+    NSUInteger numberOfMatches = [regExpThread numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
+    NSLog(@"number of matches: %lu", (unsigned long)numberOfMatches);
+    
+    if(numberOfMatches == 1)
+    {
+        NSArray *matches = [regExpThread matchesInString:line
+                                                     options:0
+                                                       range:NSMakeRange(0, [line length])];
+        
+        NSTextCheckingResult *match = [matches objectAtIndex:0];
+
+        NSRange levelRange = [match rangeAtIndex:1];
+        NSString* level = [line substringWithRange:levelRange];
+        
+        NSRange pidRange = [match rangeAtIndex:2];
+        NSString* pid = [line substringWithRange:pidRange];
+        
+        NSRange tidRange = [match rangeAtIndex:3];
+        NSString* tid = [line substringWithRange:tidRange];
+        
+        NSRange msgRange = [match rangeAtIndex:4];
+        NSString* msg = [line substringWithRange:msgRange];
+        
+        //time, app, pid, tid, type, name, text,
+        NSArray* values = @[[self getIndex], @"", @"", pid, tid, level, @"", msg];
+        NSLog(@"entry: %@", values);
+        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
+        [self appendRow:row];
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (BOOL)tryReadWithDdmsSavePatternForLine:(NSString*)line
+{
+    NSUInteger numberOfMatches = [regExpDdmsSave numberOfMatchesInString:line options:0 range:NSMakeRange(0, line.length)];
+    NSLog(@"number of matches: %lu", (unsigned long)numberOfMatches);
+    
+    if(numberOfMatches == 1)
+    {
+        NSArray *matches = [regExpDdmsSave matchesInString:line
+                                                 options:0
+                                                   range:NSMakeRange(0, [line length])];
+        
+        NSTextCheckingResult *match = [matches objectAtIndex:0];
+        
+        NSRange timestampRange = [match rangeAtIndex:1];
+        NSString *timestamp = [line substringWithRange:timestampRange];
+        
+        NSRange levelRange = [match rangeAtIndex:4];
+        NSString* level = [line substringWithRange:levelRange];
+        
+        NSRange tagRange = [match rangeAtIndex:5];
+        NSString* tag = [line substringWithRange:tagRange];
+        
+        NSRange pidRange = [match rangeAtIndex:2];
+        NSString* pid = [line substringWithRange:pidRange];
+        
+        NSRange msgRange = [match rangeAtIndex:6];
+        NSString* msg = [line substringWithRange:msgRange];
+        
+        //time, app, pid, tid, type, name, text,
+        NSArray* values = @[[self getIndex], timestamp, @"", pid, @"", level, tag, msg];
+        NSLog(@"entry: %@", values);
+        NSDictionary* row = [NSDictionary dictionaryWithObjects:values forKeys:self.keysArray];
+        [self appendRow:row];
+        return YES;
+    }
+    
+    return NO;
+}
 @end
